537cde52d76d2ded05c8477308a59b83205aa2f2
Hsu Sandar Swe <susandarswe.mm@gmail.com>
2025-08-04 22:28:42 +0800

    Create Frontend folder

--- backend/EcommerceAPI/obj/Debug/net9.0/EcommerceAPI.AssemblyInfo.cs
+++ backend/EcommerceAPI/obj/Debug/net9.0/EcommerceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ece64658a1878d72495870df83d1477d0e3eb1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8e8363374b215fa1ecb19b0dd7b74b3c0bdc830")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
